1b993ee19e355928f40e436090e40e57ac0695a1
Daniel Montes <gdanielmontesh9@gmail.com>
2024-02-29 17:18:45 -0600

    add todo card

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { NzTypographyModule } from 'ng-zorro-antd/typography';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzSpaceModule } from 'ng-zorro-antd/space';
 import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzCardModule } from 'ng-zorro-antd/card';
 
 
 const components: any[] = [];
 const modules: any[] = [
   CommonModule,
-  NzButtonModule
+  NzTypographyModule, 
+  NzGridModule,
+  NzSpaceModule, 
+  NzButtonModule,
+  NzCardModule,
 ];
 
 
