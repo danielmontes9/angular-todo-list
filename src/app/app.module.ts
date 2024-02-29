b7b7b4a8dc42c54abecef52b2375bcf3db3bf283
Daniel Montes <gdanielmontesh9@gmail.com>
2024-02-29 16:41:27 -0600

    add shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoComponent } from './modules/todo/todo.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { TodoComponent } from './modules/todo/todo.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { NzButtonModule } from 'ng-zorro-antd/button';
+
+
+const components: any[] = [];
+const modules = [
+  CommonModule,
+  NzButtonModule
+];
+
+
+@NgModule({
+  declarations: [...components],
+  imports: [
+    ...modules
+  ],
+  exports: [
+    ...components,
+    ...modules
+  ]
+})
+export class SharedModule { }
